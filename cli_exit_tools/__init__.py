56aa36244458ffcda398066f79844b946189978a
bitranox <rnowotny1966@gmail.com>
2025-08-20 22:28:31 +0200

    restore __init__.py

---
+++ cli_exit_tools/__init__.py
@@ -0,0 +1,19 @@
+# detect test environment and add path for local testing
+# this should be the first import in __init__.py
+from lib_detect_testenv import *
+
+if is_testenv_active():
+    add_path_to_syspath(__file__)
+
+# put Your imports here
+from .cli_exit_tools import *
+
+# imports from __init__conf__ should happen after Your imports
+from . import __init__conf__
+
+__title__ = __init__conf__.title
+__version__ = __init__conf__.version
+__name__ = __init__conf__.name
+__url__ = __init__conf__.url
+__author__ = __init__conf__.author
+__author_email__ = __init__conf__.author_email
